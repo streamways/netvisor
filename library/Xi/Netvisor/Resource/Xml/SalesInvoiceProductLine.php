fc7a6664847c2ca0043fac485e4e9eba51850f59
Joel Hietanen <joel.hietanen@nyholmsolutions.fi>
2021-03-23 11:21:23 +0200

    Add setter for salesInvoiceProductLineDiscountPercentage, allow setting product indentifier type

--- library/Xi/Netvisor/Resource/Xml/SalesInvoiceProductLine.php
+++ library/Xi/Netvisor/Resource/Xml/SalesInvoiceProductLine.php
@@ -5,50 +5,71 @@ namespace Xi\Netvisor\Resource\Xml;
 use JMS\Serializer\Annotation\XmlList;
 use Xi\Netvisor\Resource\Xml\Component\AttributeElement;
 
+/**
+ * Class SalesInvoiceProductLine
+ *
+ * @package Xi\Netvisor\Resource\Xml
+ */
 class SalesInvoiceProductLine
 {
-    private $productIdentifier;
-    private $productName;
-    private $productUnitPrice;
-    private $productUnitPurchasePrice;
-    private $productVatPercentage;
-    private $salesInvoiceProductLineQuantity;
-    private $salesInvoiceProductLineDiscountPercentage;
-    private $salesInvoiceProductLineFreeText;
-    private $salesInvoiceProductLineVatSum;
-    private $salesInvoiceProductLineSum;
-    private $accountingAccountSuggestion;
-    private $skipAccrual;
-    private $productUnitName;
+	private $productIdentifier;
+	private $productName;
+	private $productUnitPrice;
+	private $productUnitPurchasePrice;
+	private $productVatPercentage;
+	private $salesInvoiceProductLineQuantity;
+	private $salesInvoiceProductLineDiscountPercentage;
+	private $salesInvoiceProductLineFreeText;
+	private $salesInvoiceProductLineVatSum;
+	private $salesInvoiceProductLineSum;
+	private $accountingAccountSuggestion;
+	private $skipAccrual;
+	private $productUnitName;
 
-    /**
-     * @XmlList(inline = true, entry = "dimension")
-     */
-    private $dimensions;
+	/**
+	 * @XmlList(inline = true, entry = "dimension")
+	 */
+	private $dimensions;
 
-    /**
-     * @param string $productIdentifier
-     * @param string $productName
-     * @param array $productUnitPrice
-     * @param string $productVatPercentage
-     * @param int $salesInvoiceProductLineQuantity
-     * @param array $additionalFields
-     */
-    public function __construct(
-        $productIdentifier,
-        $productName,
-        array $productUnitPrice,
-        $productVatPercentage,
-        $salesInvoiceProductLineQuantity,
-        array $additionalFields = []
-    ) {
-        $this->productIdentifier = new AttributeElement($productIdentifier, array('type' => 'netvisor')); // TODO: netvisor/customer.
-        $this->productName = substr($productName, 0, 200);
-        $this->productUnitPrice = new AttributeElement($productUnitPrice["price"], array('type' => $productUnitPrice["type"]));
-        $this->productVatPercentage = new AttributeElement($productVatPercentage, array('vatcode' => 'KOMY')); // TODO: different values.
-        $this->salesInvoiceProductLineQuantity = $salesInvoiceProductLineQuantity;
+	/**
+	 * @param int|string|array $productIdentifierData
+	 * @param string           $productName
+	 * @param array            $productUnitPrice
+	 * @param string           $productVatPercentage
+	 * @param int              $salesInvoiceProductLineQuantity
+	 * @param array            $additionalFields
+	 */
+	public function __construct(
+		$productIdentifierData,
+		$productName,
+		array $productUnitPrice,
+		$productVatPercentage,
+		$salesInvoiceProductLineQuantity,
+		array $additionalFields = []
+	) {
+		if (is_array($productIdentifierData))
+		{
+			if (array_key_exists("type", $productIdentifierData))
+			{
+				$type = $productIdentifierData["type"];
+			}
+			else
+			{
+				$type = "netvisor";
+			}
+			$this->productIdentifier = new AttributeElement($productIdentifierData["productIdentifier"], ["type" => $type]);
+		}
+		else
+		{
+			$this->productIdentifier = new AttributeElement($productIdentifierData, ["type" => "netvisor"]);
+		}
+		$this->productName                     = substr($productName, 0, 200);
+		$this->productUnitPrice                = new AttributeElement($productUnitPrice["price"], ["type" => $productUnitPrice["type"]]);
+		$this->productVatPercentage            = new AttributeElement($productVatPercentage, ["vatcode" => "KOMY"]); // TODO: different values.
+		$this->salesInvoiceProductLineQuantity = $salesInvoiceProductLineQuantity;
 
-		foreach ($additionalFields as $key => $value) {
+		foreach ($additionalFields as $key => $value)
+		{
 			$this->$key = $value;
 		}
 	}
@@ -56,7 +77,8 @@ class SalesInvoiceProductLine
 	/**
 	 * @param mixed $salesInvoiceProductLineFreeText
 	 */
-	public function setSalesInvoiceProductLineFreeText($salesInvoiceProductLineFreeText) {
+	public function setSalesInvoiceProductLineFreeText($salesInvoiceProductLineFreeText)
+	{
 		$this->salesInvoiceProductLineFreeText = $salesInvoiceProductLineFreeText;
 	}
 
@@ -68,6 +90,18 @@ class SalesInvoiceProductLine
 	public function setProductUnitName(string $productUnitName): self
 	{
 		$this->productUnitName = $productUnitName;
+
+		return $this;
+	}
+
+	/**
+	 * @param string|float|int|null $salesInvoiceProductLineDiscountPercentage
+	 *
+	 * @return $this
+	 */
+	public function setSalesInvoiceProductLineDiscountPercentage($salesInvoiceProductLineDiscountPercentage): self
+	{
+		$this->salesInvoiceProductLineDiscountPercentage = $salesInvoiceProductLineDiscountPercentage;
 
 		return $this;
 	}
